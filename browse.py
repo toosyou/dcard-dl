433501c913eff6b8d6c32449080a3baa0d0e26c4
Jasper Lin <jasperlin1996@gmail.com>
2020-02-02 21:03:42 +0800

    add risu.io support

--- browse.py
+++ browse.py
@@ -12,13 +12,13 @@ from bs4 import BeautifulSoup as bs
 
 import deepcard
 import progress_bar
+import risu
 
 import datetime
 
-sess = requests.Session()
-folder = './beta_2020_01_28/'
-start_time = datetime.datetime(2020, 1, 28)
-end_time = datetime.datetime(2020, 1, 28)
+folder = './risu_01/'
+start_time = datetime.datetime(2020, 1, 1)
+end_time = datetime.datetime(2020, 2, 3)
 worker_num = 4
 lock = threading.Lock()
 
@@ -32,6 +32,7 @@ class Worker(threading.Thread):
         self.total = self.queue.qsize()
         self.total_links = 0
         self.success_links = 0
+        self.is_expired = 0
 
     def run(self):
         while self.queue.qsize() > 0:
@@ -47,54 +48,76 @@ class Worker(threading.Thread):
                 log.write(url + '\n')
                 # parsing
                 soup = bs(html, 'html.parser')
-                pptcc_links = get_pptcc_links(soup)
+                # pptcc_links = get_pptcc_links(soup)
+                pptcc_links = set() # disable pptcc
+                risuio_links = get_risuio_links(soup)
                 passwd_set = get_password(soup)
-                priotiry_passwd = set()
-                self.total_links += len(pptcc_links)
-                pptcc_workers = []
+                priority_passwd = set()
+                self.total_links += len(pptcc_links) + len(risuio_links)
+                # pptcc_workers = []
                 for test_purpose in range(1): #  for test purpose, switch to linear version
-                    for pptcc_url in pptcc_links:
+                    for short_url in [*risuio_links, *pptcc_links]:
                         ret, content_type = None, None
                         try:
-                            ret, content_type = download_process(
-                                pptcc_url, priotiry_passwd, passwd_set)  # dl
+                            if 'risu' in short_url:
+                                ret, content_type = risu.risuio_dl(
+                                    folder, short_url, priority_passwd, passwd_set 
+                                )
+                            if 'ppt.cc' in short_url:
+                                ret, content_type = pptcc_dl(
+                                    shorts_url, priority_passwd, passwd_set
+                                )  # dl
+                        except Exception as e:
+                            self.lock.acquire()
+                            print(e)
+                            raise e
+                            log.write(
+                                u"[Unknown][\u001b[31mFailed\u001b[0m] " + f"{url} {short_url}\n")
+                            pb.bar_with_info(u"[Unknown][\u001b[31mFailed\u001b[0m] " +
+                                             f"{short_url}")
+                            self.lock.release()
+
+                        if ret:
+                            # logging
+                            self.success_links += 1
+                            self.lock.acquire()
+                            log.write(f"[{content_type}]" + u"[\u001b[32mSuccess\u001b[0m]" +
+                                      f"{url} {short_url} {ret}\n")
+                            pb.bar_with_info(f"[{content_type}]" + u"[\u001b[32mSuccess\u001b[0m] " +
+                                             f"{short_url} {ret}")
+                            # optimize
+                            priority_passwd.add(ret)
+                            self.lock.release()
+                        else:
+                            # logging
+                            self.lock.acquire()
+                            if content_type == 'is_expired':
+                                self.total_links -= 1
+                                self.is_expired  += 1
+                            log.write(
+                                f"[{content_type if content_type else 'Unknown'}]" + 
+                                u"[\u001b[31mFailed\u001b[0m] " + 
+                                f"{url} {short_url}\n"
+                            )
+                            pb.bar_with_info(
+                                f"[{content_type if content_type else 'Unknown'}]" +
+                                u"[\u001b[31mFailed\u001b[0m] " +
+                                f"{short_url}"
+                            )
+                            self.lock.release()
+
                 # with concurrent.futures.ThreadPoolExecutor() as executor:
                 #     future_to_url = {
-                #         executor.submit(download_process, pptcc_url, priotiry_passwd, passwd_set): pptcc_url for pptcc_url in pptcc_links
+                #         executor.submit(
+                #             download_process, short_url, priority_passwd, passwd_set
+                #         ): short_url for short_url in pptcc_links
                 #     }
                 #     for future in concurrent.futures.as_completed(future_to_url):
                 #         ret, content_type = None, None
                 #         pptcc_url = future_to_url[future]
                 #         try:
                 #             ret, content_type = future.result() # get return values
-                        except Exception as e:
-                            self.lock.acquire()
-                            print(e)
-                            log.write(
-                                u"[Unknown][\u001b[31mFailed\u001b[0m] " + f"{url} {pptcc_url}\n")
-                            pb.bar_with_info(u"[Unknown][\u001b[31mFailed\u001b[0m] " +
-                                            f"{pptcc_url}")
-                            self.lock.release()
-                    
-                        if ret:
-                            # logging
-                            self.success_links += 1
-                            self.lock.acquire()
-                            log.write(f"[{content_type}]" + u"[\u001b[32mSuccess\u001b[0m]" +
-                                    f"{url} {pptcc_url} {ret}\n")
-                            pb.bar_with_info(f"[{content_type}]" + u"[\u001b[32mSuccess\u001b[0m] " +
-                                            f"{pptcc_url} {ret}")
-                            # optimize
-                            priotiry_passwd.add(ret)
-                            self.lock.release()
-                        else:
-                            # logging
-                            self.lock.acquire()
-                            log.write(
-                                u"[Unknown][\u001b[31mFailed\u001b[0m] " + f"{url} {pptcc_url}\n")
-                            pb.bar_with_info(u"[Unknown][\u001b[31mFailed\u001b[0m] " +
-                                            f"{pptcc_url}")
-                            self.lock.release()
+                       
                 self.lock.acquire()
                 pb.bar((self.total - self.queue.qsize())/self.total)
                 self.lock.release()
@@ -125,37 +148,48 @@ def get_html(url):
         html(str): html text
     """
     ui(f'get_html: {url}')
-    res = sess.get(url)
+    res = requests.get(url)
     if res.status_code == 200:
         return res.text
     else:
         raise Exception(f'URL {url} unreachable.')
 
 
-def get_pptcc_links(soup):
+def get_short_links(soup, regex):
     """
     Args:
         soup(bs): BeautifulSoup object
+        regex(re.compile): re object
     Return:
-        links(set): ppt.cc links, str
+        links(set): short links, str
     """
-    ui(f'get_pptcc_links')
+    
     # links = soup.find_all('a', href=re.compile('.*ppt\.cc.*'))
     links = set()
     post_content_links = soup.select('div[class*=Post_content] > div > div > div > div > a')
     for a in post_content_links:
-        if 'ppt.cc' in a.text:
-            links.add(a.text)
+        match = regex.match(a.text)
+        if match:
+            links.add(match.string)
     post_authors = soup.select('span[class*=PostAuthor_root]')
     for post_author in post_authors:
         if post_author.text == '原PO':
             for parent in post_author.parents:
                 if parent.name == 'header':
-                    tmp = parent.parent.find_all('a', href=re.compile('.*ppt\.cc.*'))
+                    tmp = parent.parent.find_all('a', href=regex) # '.*ppt\.cc.*'
                     if tmp:
                         links.add(tmp[0].text)
     return links
 
+def get_pptcc_links(soup):
+    ui(f'get_pptcc_links')
+    pattern = re.compile('.*ppt\.cc.*')
+    return get_short_links(soup, pattern)
+
+def get_risuio_links(soup):
+    ui(f'get_risuio_links')
+    pattern = re.compile('.*risu\.io/[a-zA-Z]+')
+    return get_short_links(soup, pattern)
 
 def search_reply(soup):
     ui(f'search_reply')
@@ -192,7 +226,8 @@ def get_password(soup):
                       soup.select('span[class*=Post_date]')[0].text)[0]
     month, day = date[:-1].split('月')
     for date_shift in range(4):  # brute force date shift
-        date = "{:02}".format(int(month)) + "{:02}".format(int(day)+date_shift)
+        date = (datetime.date(2020, int(month), int(day))+datetime.timedelta(days=date_shift)).strftime('%m%d')
+        # date = "{:02}".format(int(month)) + "{:02}".format(int(day)+date_shift)
         passwd_set.add(date)  # dates
 
     YEARS = ['2020', '2019', '2018', '2017']
@@ -206,26 +241,25 @@ def get_password(soup):
         # post content without '-'
         passwd_set.add(i.text.strip().replace('-', ''))
     try:
-        passwd_set.remove('')  # prevent empty passwd
-    except:  # if already nice
-        pass
-    try:
         passwd_set = passwd_set.union({'0000', '1234', '4321'}) # artificial passwd
     except:
         pass
     passwd_set = set(map(lambda i: re.sub(
         '[-|#|＃|「|」|=|:|：|(Password)|(password)|密碼]', '', i.strip()).strip(), passwd_set))
+    try:
+        passwd_set.remove('')  # prevent empty passwd
+    except:  # if already nice
+        pass
     return passwd_set
 
 
-def download_process(url, priority_passwd, passwd_set):  # download single pptcc resource(img or img+mov)
+def pptcc_dl(url, priority_passwd, passwd_set):  # download single pptcc resource(img or img+mov)
     """
     Return:
         passwd(str): nice passwd
         type(str): content type
     """
     ui(f'download_process {url}')
-    sess_ = requests.Session()
 
     website_url = url
     resource_url = url + '@.jpg'
@@ -235,40 +269,40 @@ def download_process(url, priority_passwd, passwd_set):  # download single pptcc
         't': 2,
         'p': '',
     }
-    for passwd in [*priority_passwd, *passwd_set]:
-        try:
-            data['p'] = passwd
-            res_ = sess_.post(website_url, data=data)
-            soup_ = bs(res_.text, 'html.parser')
-            passwd_error_situation = [
-                'alert("請輸入您的通關密碼!");history.go(-1)', 'alert(\"您輸入的密碼並不正確，請再做檢查!\");history.go(-1)']
-            if res_.status_code == 200 and soup_.find('script').text not in passwd_error_situation:
-                res_ = sess_.get(resource_url)
-                # content is large enough to be an image or larger than default image(48373)
-                if len(res_.content) != 48373:
-                    passwd_ = str(passwd)
-                    with open(folder+url.split('/')[-1]+'_'+passwd_+'.jpg', 'wb') as f:
-                        f.write(res_.content)
+    with requests.Session() as sess_:
+        for passwd in [*priority_passwd, *passwd_set]:
+            try:
+                data['p'] = passwd
+                res_ = sess_.post(website_url, data=data)
+                soup_ = bs(res_.text, 'html.parser')
+                passwd_error_situation = [
+                    'alert("請輸入您的通關密碼!");history.go(-1)', 'alert(\"您輸入的密碼並不正確，請再做檢查!\");history.go(-1)']
+                if res_.status_code == 200 and soup_.find('script').text not in passwd_error_situation:
+                    res_ = sess_.get(resource_url)
+                    # content is large enough to be an image or larger than default image(48373)
+                    if len(res_.content) != 48373:
+                        passwd_ = str(passwd)
+                        with open(folder+url.split('/')[-1]+'_'+passwd_+'.jpg', 'wb') as f:
+                            f.write(res_.content)
 
-                    # try mov
-                    mov_sess = sess_
-                    mov_res = mov_sess.post(website_url, data=data)
+                        # try mov
+                        mov_sess = sess_
+                        mov_res = mov_sess.post(website_url, data=data)
 
-                    mov_res = mov_sess.get('https://www.ppt.cc/mov/player.php',
-                                        headers={'referer': website_url, 'range': 'bytes=0-'})
-                    if mov_res.status_code == 206:
-                        with open(folder+url.split('/')[-1]+'_'+passwd+'.mp4', 'wb') as f:
-                            f.write(mov_res.content)
-                            return passwd, 'mov'
-                    else:
-                        return passwd, 'img'
-        except ConnectionError as e:
-            print(e)
-            time.sleep(1)
-            continue
-        except Exception as e:
-            raise e
-    return None, None
+                        mov_res = mov_sess.get('https://www.ppt.cc/mov/player.php',
+                                            headers={'referer': website_url, 'range': 'bytes=0-'})
+                        if mov_res.status_code == 206:
+                            with open(folder+url.split('/')[-1]+'_'+passwd+'.mp4', 'wb') as f:
+                                f.write(mov_res.content)
+                                return passwd, 'mov'
+                        else:
+                            return passwd, 'img'
+            except ConnectionError as e:
+                print(e)
+                continue
+            except Exception as e:
+                raise e
+        return None, None
 
 
 def get_articles(url):
@@ -288,7 +322,7 @@ def get_articles(url):
 
 def test_dl_all_img():
     ##  dcard site
-    article_links = get_articles('https://www.dcard.tw/f/sex')
+    # article_links = get_articles('https://www.dcard.tw/f/sex')
 
     ##  dcard api (unable to brute force, will be blocked)
     # article_links = list(map(lambda a: str(a['id']), requests.Session().get(
@@ -296,7 +330,7 @@ def test_dl_all_img():
     # ).json()))
 
     ##  deepcard api
-    # article_links = deepcard.get_articles(start_time, end_time)
+    article_links = deepcard.get_articles(start_time, end_time)
     ##
     total_links = 0
     success_links = 0
@@ -339,50 +373,50 @@ def concurrent_download_process(url, priority_passwd, passwd_set):
 
 
 def try_passwd(url, passwd):
-    sess_ = requests.Session()
-    time.sleep(1)
-    website_url = url
-    resource_url = url + '@.jpg'
-    data = {
-        'url': website_url,
-        'ga': 1,
-        't': 2,
-        'p': '',
-    }
-    
-    try:
-        data['p'] = passwd
-        res_ = sess_.post(website_url, data=data)
-        soup_ = bs(res_.text, 'html.parser')
-        passwd_error_situation = [
-            'alert("請輸入您的通關密碼!");history.go(-1)', 'alert(\"您輸入的密碼並不正確，請再做檢查!\");history.go(-1)']
-        if res_.status_code == 200 and soup_.find('script').text not in passwd_error_situation:
-            res_ = sess_.get(resource_url)  # Disable SSL verify
-            # content is large enough to be an image or larger than default image(48373)
-            if len(res_.content) != 48373:
-                passwd_ = str(passwd)
-                with open(folder+url.split('/')[-1]+'_'+passwd_+'.jpg', 'wb') as f:
-                    f.write(res_.content)
+    with requests.Session() as sess_:
+        time.sleep(1)
+        website_url = url
+        resource_url = url + '@.jpg'
+        data = {
+            'url': website_url,
+            'ga': 1,
+            't': 2,
+            'p': '',
+        }
+        
+        try:
+            data['p'] = passwd
+            res_ = sess_.post(website_url, data=data)
+            soup_ = bs(res_.text, 'html.parser')
+            passwd_error_situation = [
+                'alert("請輸入您的通關密碼!");history.go(-1)', 'alert(\"您輸入的密碼並不正確，請再做檢查!\");history.go(-1)']
+            if res_.status_code == 200 and soup_.find('script').text not in passwd_error_situation:
+                res_ = sess_.get(resource_url)  # Disable SSL verify
+                # content is large enough to be an image or larger than default image(48373)
+                if len(res_.content) != 48373:
+                    passwd_ = str(passwd)
+                    with open(folder+url.split('/')[-1]+'_'+passwd_+'.jpg', 'wb') as f:
+                        f.write(res_.content)
 
-                # try mov
-                mov_sess = sess_
-                mov_res = mov_sess.post(website_url, data=data)
+                    # try mov
+                    mov_sess = sess_
+                    mov_res = mov_sess.post(website_url, data=data)
 
-                mov_res = mov_sess.get('https://www.ppt.cc/mov/player.php',
-                                        headers={'referer': website_url, 'range': 'bytes=0-'})
-                if mov_res.status_code == 206:
-                    with open(folder+url.split('/')[-1]+'_'+passwd+'.mp4', 'wb') as f:
-                        f.write(mov_res.content)
-                        return passwd, 'mov'
-                else:
-                    return passwd, 'img'
-    except ConnectionError as e:
-        print(e)
-        # time.sleep(random.randint(1,3))
-        # try_passwd(url, passwd)
-    except Exception as e:
-        raise e
-    return None, None
+                    mov_res = mov_sess.get('https://www.ppt.cc/mov/player.php',
+                                            headers={'referer': website_url, 'range': 'bytes=0-'})
+                    if mov_res.status_code == 206:
+                        with open(folder+url.split('/')[-1]+'_'+passwd+'.mp4', 'wb') as f:
+                            f.write(mov_res.content)
+                            return passwd, 'mov'
+                    else:
+                        return passwd, 'img'
+        except ConnectionError as e:
+            print(e)
+            # time.sleep(random.randint(1,3))
+            # try_passwd(url, passwd)
+        except Exception as e:
+            raise e
+        return None, None
 
 if __name__ == "__main__":
     create_folder(folder)
---
+++ risu.py
@@ -0,0 +1,94 @@
+import os
+import json
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+def dl(sess, folder, url, passwd, file_info):
+    file_path = file_info['file_path']
+    file_type = file_info['content_type']
+    
+    with open(folder+url.split('/')[-1]+'_'+passwd+'.'+file_type.split('/')[-1], 'wb') as f:
+        res = sess.get(file_path, verify=False, stream=True)
+        for chunk in res:
+            f.write(chunk)
+    print(file_path, file_type)
+
+    return passwd, file_type
+
+
+def get_risu_dl_link(folder: str, url: str, passwd: str) ->str:
+    """
+    url(str): risu.io file url
+    """
+    headers = {
+            # 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            # 'accept-encoding': 'gzip, deflate, br',
+            # 'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+            # 'cache-control': 'max-age=0',
+            # 'if-none-match': 'W/"671fcaf288cda14a79cdded6bbf7580b"',
+            # 'referer': 'https://risu.io/',
+            # 'sec-fetch-mode': 'navigate',
+            # 'sec-fetch-site': 'same-origin',
+            # 'sec-fetch-user': '?1',
+            # 'upgrade-insecure-requests': '1',
+            # 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+    }
+    requests.urllib3.disable_warnings()
+    with requests.Session() as sess:
+        res = sess.get(url, headers = headers, verify=False)
+
+        if res.status_code == 200:
+            soup = bs(res.text, 'html.parser')
+            
+            app = soup.select('#app')
+            if app:
+                params = json.loads(next(app[0].children)[':params'])
+                if params['lock'] == False: # if no password needed
+                    file_info = params['file_infos'][0]
+                    passwd, file_type = dl(sess, folder, url, passwd, file_info)
+                    return 'NOPASSWD', file_type
+
+            result = soup.select('meta[name=csrf-token]')
+            csrf_token = result[0]['content']
+            headers['x-csrf-token'] = csrf_token
+            res = sess.post(url+'/confirm.json', headers = headers, data={'password': str(passwd)}, verify=False)
+
+            if res.status_code == 200 and 'is_expired' in res.json():
+                return None, 'is_expired'
+            elif res.status_code == 200 and res.json()['lock'] == True:
+                file_info = res.json()['file_infos'][0]
+                passwd, file_type = dl(sess, folder, url, passwd, file_info)
+                return passwd, file_type
+            elif res.status_code == 200 and res.json()['lock'] == False:
+                return None, 'wrong_passwd'
+            else:
+                return None, res.status_code
+        else:
+            print(res.status_code, url)
+            return None, 'connection_error'
+
+
+def risuio_dl(folder, url, priority_passwd, passwd_set):
+    for passwd in [*priority_passwd, *passwd_set]:
+        ret, content_type = None, None
+        try:
+            ret, content_type = get_risu_dl_link(folder, url, passwd)
+            if ret:
+                return ret, content_type
+            elif content_type == 'wrong_passwd':
+                continue
+            elif content_type == 'is_expired':
+                return None, content_type
+            elif content_type == 'connection_error':
+                return None, content_type
+            else:
+                print(content_type)
+                return None, content_type
+        except Exception as e:
+            raise e
+            return None, None
+    return None, None
+
+if __name__ == "__main__":
+    pass
