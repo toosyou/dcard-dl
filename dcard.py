961b9c9b61aa640284442d8f3a0779cff67d4001
Jasper Lin <jasperlin1996@gmail.com>
2020-03-11 13:08:12 +0800

    Add  argparse support

--- dcard.py
+++ dcard.py
@@ -10,6 +10,7 @@ import queue
 import threading
 import argparse
 import pathlib
+import concurrent.futures
 
 import risu
 import pptcc
@@ -26,7 +27,7 @@ def get_pop(proxy, forums='sex'):
         article_links(list[str]): Pop posts' id.
     """ 
     return list(map(lambda a: str(a['id']), requests.get(
-        Dcard.API_ROOT + '/forums/' + forums + '/posts?popular=true&limit=30',
+        Dcard.API_ROOT + '/forums/' + forums + '/posts?popular=true&limit=100',
         proxies = proxy
     ).json()))
 
@@ -75,6 +76,7 @@ class Dcard:
             article_content = set(self.article_json["content"].split())
             host_comments   = self.get_host_comments() # depends on commentCount
             self.text_by_host = article_content.union(host_comments)
+            self.text_by_female = self.get_female_comments()
             self.short_links = {
                 'risu.io': self.get_short_links(re.compile('.*risu\.io/[a-zA-Z]+')),
                 'ppt.cc':  self.get_short_links(re.compile('.*ppt\.cc.*')),
@@ -106,7 +108,7 @@ class Dcard:
         # Regular comments
         for count in range(0, self.article_json["commentCount"], 100):
             # Be kind to Dcard API
-            time.sleep(random.random() + 1)
+            # time.sleep(random.random() + 1)
 
             res = requests.get(
                 Dcard.API_ROOT + '/posts/' + self.article_id + '/comments?limit=100&after='+str(count),
@@ -118,6 +120,31 @@ class Dcard:
         
         return comments
 
+    def get_female_comments(self):
+        """
+        Description:
+            Get comments send by host.
+        Args:
+            article_id(str): Just id -3-.
+        Return:
+            content(set[str]): Comments' content, split by spaces etc.
+        """
+        comments = set()
+
+        # Regular comments
+        for count in range(0, self.article_json["commentCount"], 100):
+            # Be kind to Dcard API
+            # time.sleep(random.random() + 1)
+
+            res = requests.get(
+                Dcard.API_ROOT + '/posts/' + self.article_id + '/comments?limit=100&after='+str(count),
+                proxies = self.proxy
+            )
+            for res_json in res.json():
+                if res_json['hidden'] == False and res_json['gender'] == 'F':
+                    comments = comments.union(set(res_json['content'].split()))
+        
+        return comments
 
     def get_short_links(self, regex):
         """
@@ -133,6 +160,9 @@ class Dcard:
         """
         links = set()
         for line in self.text_by_host:
+            if regex.match(line):
+                links.add(line)
+        for line in self.text_by_female:
             if regex.match(line):
                 links.add(line)
         
@@ -160,7 +190,7 @@ class Dcard:
                 passwd_set.add(candidate_passwd)
         
         year, month, day = self.article_json['createdAt'].split('T')[0].split('-')
-        for date_shift in range(4): # Maybe try createdAt -> updatedAt
+        for date_shift in range(6): # Maybe try createdAt -> updatedAt
             date = (datetime.date(int(year), int(month), int(day))+datetime.timedelta(days=date_shift)).strftime('%m%d')
             passwd_set.add(date)
         
@@ -209,7 +239,7 @@ class Worker(threading.Thread):
                 # Be kind to dcard'api
                 # Sleep after get id from queue, in case of 
                 # calling `.get()` to an empty queue
-                time.sleep(random.random() + 1)
+                # time.sleep(random.random() + 1)
                 # Create Dcard object
                 try:
                     dcard = Dcard(article_id, self.proxy)
@@ -224,7 +254,20 @@ class Worker(threading.Thread):
                 priority_passwd = set()
                 self.total_links += len(dcard.short_links['risu.io'])*service_status['risu.io'] + \
                                     len(dcard.short_links['ppt.cc'])*service_status['ppt.cc']
-                                   
+
+                ## Threading
+                # with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+                #     future_to_url = {
+                #         executor.submit(
+                #             download_process, url, priority_passwd, passwd_set
+                #         ): url for url in dcard.short_links.values()
+                #     }
+                #     for future in concurrent.futures.as_completed(future_to_url):
+                #         ret, content_type = None, None
+                #         url = future_to_url[future]
+                #         try:
+                #             ret, content_type = future.result()
+                # ##
                 for service, short_url in dcard.short_links.items():
                     for url in short_url:
                         ret, content_type = None, None
@@ -296,11 +339,15 @@ if __name__ == "__main__":
     time_group.add_argument('-r', '--range', help='set range(use deepcard api) format: YYYY_MM_DD_YYYY_MM_DD')
     parser.add_argument('-f', '--folder', help='set folder path(will auto-generate it for u)')
     parser.add_argument('-t', '--thread', help='default = 4', type=int, default=4)
+    parser.add_argument('--proxy', help='set proxy.json path', type=str, default='./proxy.json')
     args = parser.parse_args()
     folder = args.folder or folder
     folder = os.path.join('./', folder, '')
+    proxy_path = args.proxy
     # Create folder
     create_folder(folder)
+    with open(folder + 'exe_date.log', 'a') as f:
+        f.write(str(datetime.datetime.now())+'\n')
 
     if args.range:
         time_range = list(map(int, args.range.split('_')))
@@ -311,7 +358,7 @@ if __name__ == "__main__":
     worker_num = args.thread
     # Use proxy
     try:
-        with open('proxy.json', 'r') as j:
+        with open(proxy_path, 'r') as j:
             proxy = json.loads(j.read())
             if len(proxy) > 0:
                 select = random.randint(1, len(proxy)) - 1
@@ -349,4 +396,4 @@ if __name__ == "__main__":
         print(f"Total links: {total_links}")
         print(f"Expired links: {expired_links}")
         print(f"Success links: {success_links}")
-        print(f"Success rate: {success_links/total_links}")
+        print(f"Success rate: {success_links/(total_links-expired_links)}")
